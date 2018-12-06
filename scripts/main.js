7d688fb1817536af6811ca3837da4cfaa6b031f0
Keenan <kpautler4@gmail.com>
2018-12-05 20:46:41 -0700

    finished most of the core mechanics to the game

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+	var wordList = ['budget', 'gift', 'password', 'familiar', 'coach', 'constituency', 'twist', 'agenda', 'read', 'tax', 'hunter', 'speculate', 'access', 'pain', 'hospital', 'late', 'radiation', 'sign', 'pay', 'introduce', 'pursuit', 'biscuit', 'belly', 'cabin', 'Europe', 'uncle', 'shoot', 'infect', 'wound', 'essay'];
+
+	var word = wordList[Math.floor(Math.random()*wordList.length)]; 
+	var guessLetters = [];
+	var letters = [];
+	console.log(word);
+	for (i = 0; i < word.length; i++) {
+		$('.wordBlanks').append(`<h1 id="${word[i]}">_</h1>`);
+		letters.push(word[i]);	
+	}	
+
+	document.onkeyup = function(event) {
+		var guess = event.key;
+		
+		if (word.includes(guess)){	
+			$(`h1[id=${guess}]`).text(guess);	
+		}
+		else if (!guessLetters.includes(guess)){
+			$(".guessedLetters").append(`<p>${guess}</p>`)
+			guessLetters.push(guess);
+		}
+		
+	}
+});
